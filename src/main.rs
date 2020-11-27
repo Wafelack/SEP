a6ac6a1a264ace2c47626b4c11ea614c905f0785
Wafelack <wafelack@protonmail.com>
2020-11-27 18:51:35 +0100

    init: initial commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use colored::*;
+use std::collections::BTreeMap;
+use std::net::TcpStream;
+
+fn help() {
+    eprintln!("Usage : scanports $IP $FIRST_PORT-$LAST_PORT");
+}
+
+fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() != 3 {
+        help();
+        std::process::exit(-1);   
+    }
+    let ip = &args[1].as_str();
+    let (start, end): (i32, i32) = {
+        let ports = &args[2].split('-').collect::<Vec<&str>>();
+
+        if ports.len() != 2 {
+            eprintln!("Invalid ports");
+            help();      
+            std::process::exit(-1);   
+        }
+        let start = match ports[0].parse::<i32>() {
+            Ok(i) => i,
+            Err(_e) => {
+                eprintln!("Ports have to be valid integers");
+                help();
+                std::process::exit(-1);   
+            }
+        };
+        let end = match ports[1].parse::<i32>() {
+            Ok(i) => i,
+            Err(_e) => {
+                eprintln!("Ports have to be valid integers");
+                help();
+                std::process::exit(-1);   
+            }
+        };
+        (start, end + 1)
+    };
+
+    let mut ports = BTreeMap::new();
+    let mut open = 0usize;
+    let mut closed = 0usize;
+
+    for port in start..end {
+        if let Ok(_stream) = TcpStream::connect(format!("{}:{}", ip, port)) {
+            ports.insert(port, "Open".green());
+            open+=1;
+        } else {
+            ports.insert(port, "Closed".red());
+            closed+=1;
+        }  
+    }
+
+    println!("Target : {}\n", ip);
+    for (port, status) in ports.iter() {
+        println!("Port {}.....{}", port, status);
+    }
+    println!("\nTotal : {} ports : {} open, {} closed", open + closed, open, closed);
+}
