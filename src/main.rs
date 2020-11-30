41336948c8883ea73ae11904f21957924b0dad1b
Wafelack <wafelack@protonmail.com>
2020-11-30 20:20:04 +0100

    feat: finished building entire software

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::{Arg, App};
 use colored::*;
 use std::collections::BTreeMap;
 use std::net::TcpStream;
+use std::time::Instant;
 
 fn parse_addresses(raw: &str) -> Vec<String> {
     let parts = raw.split(".").map(|s| { s.to_string() }).collect::<Vec<String>>();
@@ -19,14 +20,14 @@ fn parse_addresses(raw: &str) -> Vec<String> {
 
             let splited = parts[i].split("/").collect::<Vec<&str>>();
 
-            let first = match splited[0].parse::<i32>() {
+            let first = match splited[0].parse::<u32>() {
                 Ok(n) => n,
-                Err(_e) => 22
+                Err(_e) => 8
             };
 
-            let second = match splited[1].parse::<i32>() {
+            let second = match splited[1].parse::<u32>() {
                 Ok(n) => n,
-                Err(_e) => 22,
+                Err(_e) => 9,
             };
 
             for j in first..second {
@@ -56,13 +57,51 @@ fn parse_addresses(raw: &str) -> Vec<String> {
     ips
 }
 
+fn parse_ports(raw: &str) -> Vec<String> {
+    let parts = raw.split(",").map(|s| { s.to_string() }).collect::<Vec<String>>();
+    let mut ports: Vec<String> = vec![];
+    if parts.len() < 1 {
+        return vec![];
+    }
+    
+    for part in parts {
+        if part.split("/").map(|s| { s.to_string() }).collect::<Vec<String>>().len() == 2 {
+            let splited = part.split("/").collect::<Vec<&str>>();
+
+            let first = match splited[0].parse::<u32>() {
+                Ok(n) => n,
+                Err(_e) => 22
+            };
+
+            let second = match splited[1].parse::<u32>() {
+                Ok(n) => n,
+                Err(_e) => 23,
+            };
+
+            for i in first..second {
+                ports.push(format!("{}", i));
+            }
+        } else {
+            ports.push(part);
+        }
+    }
+
+    ports
+
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn parser() {
+    fn add_rparser() {
         assert_eq!(parse_addresses("127.0.0/2.1/3"), vec!["127.0.0.1", "127.0.0.2", "127.0.1.1", "127.0.1.2"]);
+    }
+
+    #[test]
+    fn ports_parser() {
+        assert_eq!(parse_ports("55,5/8"), vec!["55", "5", "6", "7"]);
     }
 
 }
@@ -80,67 +119,57 @@ fn main() {
                         .long("ports")
                         .value_name("PORTS")
                         .help("Sets the ports to scan")
-                        .takes_value(true))
-                    .arg(Arg::with_name("address")
+                        .takes_value(true)
+                        .required(true))
+                    .arg(Arg::with_name("addresses")
                         .short("i")
-                        .long("ip")
+                        .long("ips")
                         .value_name("ADDRESS")
                         .help("Sets the addresses to scan")
                         .takes_value(true)
                         .required(true))
                     .get_matches();
 
+        let ips = parse_addresses(matches.value_of("addresses").unwrap());
+        let ports = parse_ports(matches.value_of("ports").unwrap());
 
-    /*
-    let ip = &args[1].as_str();
-    let (start, end): (i32, i32) = {
-        let ports = &args[2].split('-').collect::<Vec<&str>>();
+        let mut total = 0usize;
 
-        if ports.len() != 2 {
-            eprintln!("Invalid ports");
-            help();      
-            std::process::exit(-1);   
+        let start = Instant::now();
+
+        for ip in ips {
+
+            let mut status: BTreeMap<&String, colored::ColoredString> = BTreeMap::new();
+
+            let mut open_amount = 0usize;
+            let mut closed = 0usize;
+
+            for port in &ports {
+                let open = match TcpStream::connect(&format!("{}:{}", ip, port)) {
+                    Ok(_) => true,
+                    Err(_e) => false,
+                };
+
+                if open {
+                    status.insert(port, "Open".green());
+                    open_amount+=1;
+                } else {
+                    status.insert(port, "Closed".red());
+                    closed+=1;
+                }
+            }
+
+            
+
+            println!("Target address : {}\n", &ip);
+            total+=1;
+
+            for (port, stat) in status.iter() {
+                println!("{}.....{}", port, stat);
+            }
+            println!("\nTotal : {} ports scanned : {} open, {} closed.", open_amount + closed, open_amount, closed);
+            println!("\n======================================================\n");
         }
-        let start = match ports[0].parse::<i32>() {
-            Ok(i) => i,
-            Err(_e) => {
-                eprintln!("Ports have to be valid integers");
-                help();
-                std::process::exit(-1);   
-            }
-        };
-        let end = match ports[1].parse::<i32>() {
-            Ok(i) => i,
-            Err(_e) => {
-                eprintln!("Ports have to be valid integers");
-                help();
-                std::process::exit(-1);   
-            }
-        };
-        (start, end + 1)
-    };
-
-    let mut ports = BTreeMap::new();
-    let mut open = 0usize;
-    let mut closed = 0usize;
-
-    let center = end / 2;
-
-
-
-    for port in start..end {
-        if let Ok(_stream) = TcpStream::connect(format!("{}:{}", ip, port)) {
-            ports.insert(port, "Open".green());
-            open+=1;
-        } else {
-            ports.insert(port, "Closed".red());
-            closed+=1;
-        }  
+        let elapsed = start.elapsed();
+        println!("\n{} address scanned in {:.2?}", total, elapsed);
     }
-
-    println!("Target : {}\n", ip);
-    for (port, status) in ports.iter() {
-        println!("Port {}.....{}", port, status);
-    }
-    println!("\nTotal : {} ports : {} open, {} closed", open + closed, open, closed);
-*/}
