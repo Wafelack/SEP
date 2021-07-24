65d8395b7eb3dc0bd941e0fe2ce5a640f1301817
Wafelack <wafelack@protonmail.com>
2021-07-24 18:26:40 +0200

    misc: ignore target.

--- sep/target/debug/deps/sep-9cb2694e5097db6e.d
+++
@@ -1,5 +0,0 @@
-/home/wafelack/code/SEP/sep/target/debug/deps/sep-9cb2694e5097db6e.rmeta: src/lib.rs
-
-/home/wafelack/code/SEP/sep/target/debug/deps/sep-9cb2694e5097db6e.d: src/lib.rs
-
-src/lib.rs:
--- sep/target/debug/deps/sep-f3805314564f16ac.d
+++
@@ -1,5 +0,0 @@
-/home/wafelack/code/SEP/sep/target/debug/deps/sep-f3805314564f16ac.rmeta: src/lib.rs
-
-/home/wafelack/code/SEP/sep/target/debug/deps/sep-f3805314564f16ac.d: src/lib.rs
-
-src/lib.rs:
